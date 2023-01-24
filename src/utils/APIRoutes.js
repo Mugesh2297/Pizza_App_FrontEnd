ffadc62ed8ee9ee6f62028d51a799a7ab827c5cd
Mugesh <mugeshkumark22@gmail.com>
2023-01-24 12:42:59 +0530

    link changes

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 
-export const host = "http://localhost:3001";
+export const host = "https://pizza-app-back-end.vercel.app/";
 
 export const getPizzaData = `${host}/api/pizzas/getAllPizzas`;
 export const addNewPizza = `${host}/api/pizzas/addpizza`;
